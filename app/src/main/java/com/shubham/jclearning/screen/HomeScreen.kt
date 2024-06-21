e5feeef4ecf46ad98105bb4b6603504bf0bf144b
shubham <smahalkar@gmail.com>
2024-06-21 22:43:39 +0530

    added bottom sheet on click

--- app/src/main/java/com/shubham/jclearning/screen/HomeScreen.kt
+++ app/src/main/java/com/shubham/jclearning/screen/HomeScreen.kt
@@ -18,13 +18,21 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,6 +40,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,7 +53,10 @@ fun HomeScreen() {
     Scaffold(topBar = {
         TopAppBar(title = {
             Text(
-                text = "Friends List", style = TextStyle(fontSize = 18.sp), color = Color.Black
+                text = "Friends List",
+                fontWeight = FontWeight.SemiBold,
+                style = TextStyle(fontSize = 22.sp),
+                color = Color.Black
             )
         }, navigationIcon = {
             IconButton(onClick = { /*TODO*/ }) {
@@ -90,51 +102,89 @@ val friends = listOf(
 
 @Composable
 fun FriendsList(modifier: Modifier = Modifier) {
+    var selectedFriend by remember { mutableStateOf<Friends?>(null) }
+
     Column(
         modifier.fillMaxSize(), verticalArrangement = Arrangement.Top
     ) {
         LazyColumn(modifier = modifier) {
             items(friends) { friend ->
-                FriendListItem(friends = friend)
+                FriendListItem(friend) {
+                    selectedFriend = friend
+                }
             }
+        }
+        selectedFriend.let { friendData ->
+            friendData?.let { UserDetailBottomSheet(it) }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserDetailBottomSheet(user: Friends) {
+    val modalBottomSheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        onDismissRequest = { },
+        sheetState = modalBottomSheetState,
+        dragHandle = { BottomSheetDefaults.DragHandle() },
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Text(text = user.name, style = MaterialTheme.typography.labelSmall)
+            Text(text = user.email, style = MaterialTheme.typography.labelSmall)
+            Text(text = user.phoneNumber, style = MaterialTheme.typography.labelSmall)
+            Text(text = user.address, style = MaterialTheme.typography.labelSmall)
         }
     }
 }
 
 @Composable
-fun FriendListItem(friends: Friends) {
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(start = 12.dp, end = 12.dp)
-        .clickable { }
-        .indication(indication = rememberRipple(bounded = true),
-            interactionSource = remember { MutableInteractionSource() }),
-
-        verticalAlignment = Alignment.CenterVertically
+fun FriendListItem(friends: Friends, onClick: () -> Unit) {
+    Surface(
+        modifier = Modifier
+            .indication(indication = rememberRipple(bounded = true),
+                interactionSource = remember { MutableInteractionSource() })
+            .clickable(onClick = onClick)
     ) {
-        Image(
-            painter = painterResource(id = friends.profileImage),
-            contentDescription = "ProfileImage",
-            contentScale = ContentScale.Crop,
+        Row(
             modifier = Modifier
-                .clip(shape = CircleShape)
-                .size(45.dp)
-        )
-        Column(modifier = Modifier.padding(start = 12.dp, end = 12.dp, top = 8.dp, bottom = 8.dp)) {
-            Text(
-                text = friends.name,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                fontSize = 16.sp,
-                color = Color.Black
+                .fillMaxWidth()
+                .padding(start = 12.dp, end = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = friends.profileImage),
+                contentDescription = "ProfileImage",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(45.dp)
             )
-            Text(
-                text = friends.phoneNumber,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                fontSize = 14.sp,
-                color = Color.Gray
-            )
+            Column(
+                modifier = Modifier.padding(
+                    start = 12.dp, end = 12.dp, top = 8.dp, bottom = 8.dp
+                )
+            ) {
+                Text(
+                    text = friends.name,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
+                Text(
+                    text = friends.phoneNumber,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    fontSize = 14.sp,
+                    color = Color.Gray
+                )
+            }
         }
     }
 }
